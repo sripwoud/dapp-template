fa03d55cddd872547d0eb8fe7023b67b8cbb6726
sripwoud <me@sripwoud.xyz>
2025-02-20 11:51:32 +0100

    fix: polyfill with vite-plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,27 +1,28 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-const plugins = [react(), tailwindcss(), TanStackRouterVite(), tsconfigPaths()]
+const plugins = [
+  // alternative workaround to https://docs.dynamic.xyz/troubleshooting/react/vitejs-polyfills-necessary-for-dynamic-sdk
+  nodePolyfills({
+    globals: {
+      process: true,
+    },
+  }),
+  react(),
+  tailwindcss(),
+  TanStackRouterVite(),
+  tsconfigPaths(),
+];
 
 export default defineConfig(({ mode }) => {
   return {
     // https://vite.dev/guide/static-deploy#github-pages
-    base: '/web-app-template/',
+    base: "/web-app-template/",
     plugins,
-    optimizeDeps: {
-      esbuildOptions: {
-        // https://docs.dynamic.xyz/troubleshooting/react/vitejs-polyfills-necessary-for-dynamic-sdk
-        plugins: [
-          NodeGlobalsPolyfillPlugin({ process: true }),
-          NodeModulesPolyfillPlugin(),
-        ],
-      },
-    },
-    server: { open: mode === 'development' },
-  }
-})
+    server: { open: mode === "development" },
+  };
+});
